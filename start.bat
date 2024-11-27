fbd18c5d8460ab2e39b971ddabf4292321a147e0
JINCERO <romeobluesky@gmail.com>
2024-11-27 09:20:25 +0900

    Update start.bat & .gitignore

--- start.bat
+++ start.bat
@@ -4,32 +4,32 @@
 echo Starting Nginx...
 nginx\nginx.exe -c nginx\conf\nginx.conf
 if %ERRORLEVEL% neq 0 (
-    echo Failed to start Nginx.
-    exit /b
+	echo Failed to start Nginx.
+	exit /b
 )
 
 :: MongoDB 시작
 echo Starting MongoDB...
 mongodb\bin\mongod.exe --dbpath mongodb\data --logpath mongodb\log\mongodb.log --port 27017 --bind_ip 127.0.0.1 --fork
 if %ERRORLEVEL% neq 0 (
-    echo Failed to start MongoDB.
-    exit /b
+	echo Failed to start MongoDB.
+	exit /b
 )
 
 :: NestJS npm 시작
 echo Starting NestJS...
 npm --prefix D:\work_vsc\packetwave_client run start:dev
 if %ERRORLEVEL% neq 0 (
-    echo Failed to start NestJS
-    exit /b
+	echo Failed to start NestJS
+	exit /b
 )
 
 :: Python 스키마 초기화 및 프로그램 실행
 echo Initializing MongoDB schemas...
 python apply_schemas.py
 if %ERRORLEVEL% neq 0 (
-    echo Failed to initialize schemas.
-    exit /b
+	echo Failed to initialize schemas.
+	exit /b
 )
 
 echo Starting Python program...
