0735335445e4af7d459f8e57dbbe8755ac21cd87
JINCERO <romeobluesky@gmail.com>
2024-11-27 09:15:18 +0900

    Update start.bat & .gitignore

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 
 :: Nginx 시작
 echo Starting Nginx...
-packet_wave\nginx\nginx.exe -c packet_wave\nginx\conf\nginx.conf
+nginx\nginx.exe -c nginx\conf\nginx.conf
 if %ERRORLEVEL% neq 0 (
     echo Failed to start Nginx.
     exit /b
@@ -10,7 +10,7 @@ if %ERRORLEVEL% neq 0 (
 
 :: MongoDB 시작
 echo Starting MongoDB...
-packet_wave\mongodb\bin\mongod.exe --dbpath packet_wave\mongodb\data --logpath packet_wave\mongodb\log\mongodb.log --port 27017 --bind_ip 127.0.0.1 --fork
+mongodb\bin\mongod.exe --dbpath mongodb\data --logpath mongodb\log\mongodb.log --port 27017 --bind_ip 127.0.0.1 --fork
 if %ERRORLEVEL% neq 0 (
     echo Failed to start MongoDB.
     exit /b
@@ -18,11 +18,11 @@ if %ERRORLEVEL% neq 0 (
 
 :: Python 스키마 초기화 및 프로그램 실행
 echo Initializing MongoDB schemas...
-python packet_wave\apply_schemas.py
+python apply_schemas.py
 if %ERRORLEVEL% neq 0 (
     echo Failed to initialize schemas.
     exit /b
 )
 
 echo Starting Python program...
-python packet_wave\dashboard.py
+python dashboard.py
